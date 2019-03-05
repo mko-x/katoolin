25f6b6725e337d113505a5173cdebdd87de9e71a
mko-x <github@m-ko.de>
2019-03-05 17:11:50 +0100

    rm shebang

--- download_latest.sh
+++ download_latest.sh
@@ -1,8 +1,6 @@
-#!/usr/bin/env bash sh
 
 curl -s https://api.github.com/repos/mko-x/katoolin/releases/latest \
 | grep "browser_download_url.*deb" \
 | cut -d : -f 2,3 \
 | tr -d \" \
 | wget -qi -
-
